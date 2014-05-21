cc5841a8b19b02f70a3e03fe40fac735aaff9a56
clgt <clgt@clgt.vn>
2014-05-22 06:16:28 +0700

    Update

--- routes/upload.js
+++ routes/upload.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var when = require('when');
 
 var UploadHandler = function(req, res, callback) {
 	this.req = req;
@@ -6,12 +7,52 @@ var UploadHandler = function(req, res, callback) {
 	this.callback = callback;
 };
 
+UploadHandler.prototype.setNoCacheHeaders = function() {
+	this.res.setHeader('Pragma', 'no-cache');
+    this.res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+    this.res.setHeader('Content-Disposition', 'inline; filename="files.json"');
+};
+
+UploadHandler.prototype.uploadTo = function(des) {
+	var deferreds = [];
+
+	Object.keys(this.req.files).forEach(function(field){
+		deferred.push((function(){
+			var deferred = when.defer();
+
+			require('fs').rename(
+				req.files[field].path,
+				des + req.files[field].name,
+				function(error) {
+					if(error) {
+						deferred.reject(error);
+					} else {
+						deferred.resolve(req.files[field].name);
+					}
+				}
+			);
+
+			return deferred.promise;
+		})());
+	});
+
+	return when.all(deferreds);
+};
+
 module.exports = function(app, config) {
 	var auth = app.get('auth');
 
 	app.post('/upload', auth.requireLogin, function(req, res, next){
-		var tmp_path = path.join(config.root, 'tmp');
+		// var tmp_path = path.join(config.root, 'tmp');
 
-		
+		var uploadHandler = new UploadHandler(req, res, next);
+			uploadHandler.setNoCacheHeaders();
+
+			uploadHandler.uploadTo(path.join(config.root, 'static'))
+				.then(function(fileName){
+					console.log(fileName);
+				}, function(err){
+
+				});
 	});
-}
+};
